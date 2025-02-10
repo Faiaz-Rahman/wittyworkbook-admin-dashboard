2cfb362b9d873a3e946ceaf91f1f3e0ec265210b
Mohammad Fiaz Rahman Nafee <faiazrahman70@gmail.com>
2025-02-10 08:48:21 +0600

    ui update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,17 @@
 import React from "react";
 
 import { redirect, useRouter } from "next/navigation";
+import { LoaderCircle } from "lucide-react";
 
 export default function Home() {
   const router = useRouter();
 
   // redirect("/dashboard");
   router.push("/auth/login");
-  return <>Coming Soon</>;
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <LoaderCircle className="mr-2 size-4 animate-spin" />
+      <p className="text-black font-medium text-md">Getting User Status ...</p>
+    </div>
+  );
 }
